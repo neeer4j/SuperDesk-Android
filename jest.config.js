e1123f3102e92f4d4cc8f63582d9355c243db27e
neeer4j <171962526+neeer4j@users.noreply.github.com>
2026-06-09 13:25:36 +0530

    test: setup jest configuration and mock native modules

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@gluestack-ui|lucide-react-native|react-native-reanimated|react-native-gesture-handler)/)'
+  ],
 };
